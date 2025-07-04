2a8eca5d0b86d2a5bb57c2e5e9a2e0d4cc74c2aa
tj <tuo70963@temple.edu>
2025-07-03 22:07:36 -0400

    filter hardcoded

--- main.js
+++ main.js
@@ -6,44 +6,51 @@ selection.sum = 5;
 selection.digits = 2;
 selection.results = SUMS[selection.digits][selection.sum];
 
-selection.with = [];
+selection.with = [1];
 selection.without = [2];
+
 selection.show = [];
 selection.eliminated = [];
+
+function handleResultChange(){
+    selection.show = [];
+    selection.eliminated = [];
+
+    // filter results into show and eliminated lists
+    selection.results.map((sum) => {
+        if (filter(sum)) {
+            selection.show.push(sum);
+        } else {
+            selection.eliminated.push(sum);
+        }
+    })
+
+    // update component
+    makeResults();
+}
 
 function makeResults(){
     const newDiv = document.createElement("div");
     newDiv.id = "results";
     newDiv.classList.add("results");
     const h3 = document.createElement("h3");
-    h3.innerText = `${selection.sum}/${selection.digits} (${selection.results.length} Results)`
+    h3.innerText = `${selection.sum}/${selection.digits} (${selection.show.length} Results)`
     newDiv.appendChild(h3);
-    selection.results.map((sum) => {
+    // "show" list of valid filtered results first
+    selection.show.map((sum) => {
         const p = document.createElement("p");
         p.innerText = sum;
         newDiv.appendChild(p);
     });
-
+    // "eliminated" list shows invalid answers in red below valid ones
+    selection.eliminated.map((sum) => {
+        const p = document.createElement("p");
+        p.innerText = sum;
+        p.classList.add("eliminated"); // red text
+        newDiv.appendChild(p);
+    });
     const options = document.getElementById("results");
     options.replaceWith(newDiv)
-}
-
-document.getElementById("search-form").onsubmit = (event) => {
-    event.preventDefault();
-
-    const sum = event.target.sum.value || 5;
-    const digits = event.target.digits.value || 2;
-
-    const data = SUMS[digits][sum]
-
-    if (data) {
-        selection.sum = sum;
-        selection.digits = digits;
-        selection.results = data;
-        makeResults();
-    } else {
-        alert("no results found");
-    }
 }
 
 function filter(string){
@@ -58,7 +65,25 @@ function filter(string){
     return true;
 }
 
-makeResults();
-selection.results.map((result) => {
-    console.log(filter(result))
-})
+document.getElementById("search-form").onsubmit = (event) => {
+    event.preventDefault();
+
+    const sum = event.target.sum.value || 5;
+    const digits = event.target.digits.value || 2;
+
+    const data = SUMS[digits][sum]
+
+    if (data) {
+        selection.sum = sum;
+        selection.digits = digits;
+        selection.results = data;
+
+        //selection.with = [];
+        //selection.without = [];
+        handleResultChange();
+    } else {
+        alert("no results found");
+    }
+}
+
+handleResultChange();

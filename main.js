92fb8cd07be1609bf73b09f5c239a3196872c1ff
tj <tuo70963@temple.edu>
2025-07-03 23:23:06 -0400

    full filter functionality

--- main.js
+++ main.js
@@ -6,8 +6,8 @@ selection.sum = 5;
 selection.digits = 2;
 selection.results = SUMS[selection.digits][selection.sum];
 
-selection.with = [1];
-selection.without = [2];
+selection.with = [];
+selection.without = [];
 
 selection.show = [];
 selection.eliminated = [];
@@ -65,6 +65,63 @@ function filter(string){
     return true;
 }
 
+function clearCheckboxes(){
+    selection.with = [];
+    selection.without = [];
+
+    // probably unoptimal
+    // setting 'checked' to false on each element did not uncheck
+    makeCheckboxes();
+}
+
+function makeCheckboxes(){
+    const checkboxes = document.createElement("div");
+    checkboxes.id = "checkboxes";
+    checkboxes.classList.add("checkboxes");
+
+    ["with","without"].map((type) => {
+        const div = document.createElement("div");
+        div.id = type;
+        div.classList.add(type);
+        const h3 = document.createElement("h3");
+        h3.innerText = type;
+        div.appendChild(h3);
+
+        [1,2,3,4,5,6,7,8,9].map((num) => {
+            const checkbox = makeCheckbox(type,num)
+            div.appendChild(checkbox);
+        })
+
+        checkboxes.appendChild(div);
+    })
+
+    document.getElementById("checkboxes").replaceWith(checkboxes);
+}
+
+function makeCheckbox(type,num){
+    const p = document.createElement("p");
+    p.innerText = num;
+    const input = document.createElement("input");
+    input.type = "checkbox";
+    input.classList.add("checkbox");
+    input.onchange = (event) => {
+        if (event.target.checked){
+            // add number to appropriate array
+            selection[type].push(num);
+            console.log(selection[type]);
+        } else {
+            // remove number to appropriate array
+            selection[type] = selection[type].filter((number) => {
+                return number !== num;
+            })
+            console.log(selection[type]);
+        }
+        handleResultChange();
+    }
+    p.appendChild(input);
+    return p;
+}
+
 document.getElementById("search-form").onsubmit = (event) => {
     event.preventDefault();
 
@@ -78,12 +135,17 @@ document.getElementById("search-form").onsubmit = (event) => {
         selection.digits = digits;
         selection.results = data;
 
-        //selection.with = [];
-        //selection.without = [];
+        clearCheckboxes();
         handleResultChange();
     } else {
         alert("no results found");
     }
 }
 
+document.getElementById("clear-button").onclick = () => {
+    clearCheckboxes();
+    handleResultChange();
+}
+
+makeCheckboxes();
 handleResultChange();
